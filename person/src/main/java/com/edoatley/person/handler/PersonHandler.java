d94fcf615b11ca5c7113b59d9d2fe3443bacb880
edoatley <edoatley@gmail.com>
2020-12-30 19:13:17 +0000

    tests passing

--- person/src/main/java/com/edoatley/person/handler/PersonHandler.java
+++ person/src/main/java/com/edoatley/person/handler/PersonHandler.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -30,20 +29,22 @@ public class PersonHandler {
     private final PersonRepository personRepository;
 
 
-    // TODO: trying to make not found scenario work. Not sure why fromPublisher seems to be required...
     public Mono<ServerResponse> createPerson(ServerRequest request) {
         log.info("Creating a person");
         Mono<Person> personMono = request.bodyToMono(Person.class);
         return ServerResponse.status(HttpStatus.CREATED)
-                .body(BodyInserters.fromPublisher(
-                        personMono.flatMap(personRepository::save), Person.class))
+                    .body(personMono.flatMap(personRepository::save), Person.class)
+//                .body(BodyInserters.fromPublisher(
+//                        personMono.flatMap(personRepository::save), Person.class))
                 .subscribeOn(APPLICATION_SCHEDULER);
     }
 
+    // TODO: trying to make not found scenario work. Seem to need to access the people mono
+    //       to allow switchIfEmpty to work
     public Mono<ServerResponse> getPersonById(ServerRequest request) {
         log.info("Finding a person by id");
-        return personRepository.findById(request.pathVariable("id"))
-                .flatMap(person -> ok().body(person, Person.class))
+        Mono<Person> person = personRepository.findById(request.pathVariable("id"));
+        return person.flatMap(p -> ok().body(person, Person.class))
                 .switchIfEmpty(notFound().build())
                 .subscribeOn(APPLICATION_SCHEDULER);
     }
@@ -59,9 +60,30 @@ public class PersonHandler {
     public Mono<ServerResponse> peopleByName(ServerRequest serverRequest) {
         log.debug("Finding person by name");
         String name = serverRequest.queryParam("name")
-                .orElseThrow(()->  new IllegalArgumentException("No name provided to search for"));
-        return ok()
-                .body(personRepository.findByName(name), Person.class)
-                .subscribeOn(APPLICATION_SCHEDULER);
+                .orElseThrow(() -> new IllegalArgumentException("No name provided to search for"));
+        Flux<Person> people = personRepository.findByName(name);
+
+        // can't use switchIfEmpty as the empty list is not considered empty
+        return people.hasElements()
+                .flatMap(found -> {
+                    if (found) {
+                        return ok().body(people, Person.class);
+                    } else {
+                        return notFound().build();
+                    }
+                }).subscribeOn(APPLICATION_SCHEDULER);
     }
+
+    // TODO: can we write a function to make the found / not found logic read nicely and avoid duplication
+//    <T> Mono<ServerResponse> returnListOrNotFound(Flux<T> result) {
+//        return result.hasElements()
+//                .flatMap(found -> {
+//                    if (found) {
+//                        return ok().body(result, Person.class);
+//                    } else {
+//                        return notFound().build();
+//                    }
+//                }).subscribeOn(APPLICATION_SCHEDULER);
+//    }
+
 }
